0e68d5e69a4217ae1486c5e78f42f6b13f05be6b
MHendriF <hendrifebriansyah28@gmail.com>
2024-05-30 14:35:13 +0700

    grouping routes

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,40 @@ Route::get('/g/{group:slug}', [Controllers\GroupController::class, 'profile'])->
 Route::get('/group/approve-invitation/{token}', [Controllers\GroupController::class, 'approveInvitation'])->name('group.approveInvitation');
 
 Route::middleware('auth')->group(function () {
-    Route::post('/profile/update-images', [Controllers\ProfileController::class, 'updateImage'])->name('profile.updateImages');
-    Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/user/follow/{user}', [Controllers\UserController::class, 'follow'])->name('user.follow');
 
-    Route::post('/group', [Controllers\GroupController::class, 'store'])->name('group.create');
-    Route::put('/group/{group:slug}', [Controllers\GroupController::class, 'update'])->name('group.update');
-    Route::post('/group/update-images/{group:slug}', [Controllers\GroupController::class, 'updateImage'])->name('group.updateImages');
-    Route::post('/group/invite/{group:slug}', [Controllers\GroupController::class, 'inviteUsers'])->name('group.inviteUsers');
-    Route::post('/group/join/{group:slug}', [Controllers\GroupController::class, 'join'])->name('group.join');
-    Route::post('/group/approve-request/{group:slug}', [Controllers\GroupController::class, 'approveRequest'])->name('group.approveRequest');
-    Route::post('/group/change-role/{group:slug}', [Controllers\GroupController::class, 'changeRole'])->name('group.changeRole');
-    Route::delete('/group/remove-user/{group:slug}', [Controllers\GroupController::class, 'removeUser'])->name('group.removeUser');
+    Route::prefix('/profile')->group(function () {
+        Route::post('/update-images', [Controllers\ProfileController::class, 'updateImage'])->name('profile.updateImages');
+        Route::patch('/', [Controllers\ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 
-    Route::get('/post/{post}', [Controllers\PostController::class, 'view'])->name('post.view');
-    Route::post('/post', [Controllers\PostController::class, 'store'])->name('post.create');
-    Route::put('/post/{post}', [Controllers\PostController::class, 'update'])->name('post.update');
-    Route::delete('/post/{post}', [Controllers\PostController::class, 'destroy'])->name('post.destroy');
-    Route::get('/post/download/{attachment}', [Controllers\PostController::class, 'downloadAttachment']) ->name('post.download');
-    Route::post('/post/{post}/reaction', [Controllers\PostController::class, 'postReaction'])->name('post.reaction');
+    Route::prefix('/group')->group(function () {
+        Route::post('/', [Controllers\GroupController::class, 'store'])->name('group.create');
+        Route::put('/{group:slug}', [Controllers\GroupController::class, 'update'])->name('group.update');
+        Route::post('/update-images/{group:slug}', [Controllers\GroupController::class, 'updateImage'])->name('group.updateImages');
+        Route::post('/invite/{group:slug}', [Controllers\GroupController::class, 'inviteUsers'])->name('group.inviteUsers');
+        Route::post('/join/{group:slug}', [Controllers\GroupController::class, 'join'])->name('group.join');
+        Route::post('/approve-request/{group:slug}', [Controllers\GroupController::class, 'approveRequest'])->name('group.approveRequest');
+        Route::post('/change-role/{group:slug}', [Controllers\GroupController::class, 'changeRole'])->name('group.changeRole');
+        Route::delete('/remove-user/{group:slug}', [Controllers\GroupController::class, 'removeUser'])->name('group.removeUser');
+    });
 
-    Route::post('/post/{post}/comment', [Controllers\PostController::class, 'createComment'])->name('comment.create');
-    Route::delete('/comment/{comment}', [Controllers\PostController::class, 'deleteComment'])->name('comment.delete');
-    Route::put('/comment/{comment}', [Controllers\PostController::class, 'updateComment'])->name('comment.update');
-    Route::post('/comment/{comment}/reaction', [Controllers\PostController::class, 'commentReaction'])->name('comment.reaction');
+    Route::prefix('/post')->group(function () {
+        Route::get('/{post}', [Controllers\PostController::class, 'view'])->name('post.view');
+        Route::post('', [Controllers\PostController::class, 'store'])->name('post.create');
+        Route::put('/{post}', [Controllers\PostController::class, 'update'])->name('post.update');
+        Route::delete('/{post}', [Controllers\PostController::class, 'destroy'])->name('post.destroy');
+        Route::get('/download/{attachment}', [Controllers\PostController::class, 'downloadAttachment']) ->name('post.download');
+        Route::post('/{post}/reaction', [Controllers\PostController::class, 'postReaction'])->name('post.reaction');
+        Route::post('/{post}/comment', [Controllers\PostController::class, 'createComment'])->name('comment.create');
+    });
+
+    Route::prefix('/comment')->group(function () {
+        Route::delete('/{comment}', [Controllers\PostController::class, 'deleteComment'])->name('comment.delete');
+        Route::put('/{comment}', [Controllers\PostController::class, 'updateComment'])->name('comment.update');
+        Route::post('/{comment}/reaction', [Controllers\PostController::class, 'commentReaction'])->name('comment.reaction');
+    });
 });
 
 require __DIR__.'/auth.php';
